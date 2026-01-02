86555cc4673b5ebc2f5e539e5d8b70cdefd4d446
riko303 <riko_line_303@yahoo.co.jp>
2026-01-02 22:57:51 +0900

    script.js を作成

---
+++ script.js
@@ -0,0 +1,26 @@
+document.getElementById("saveBtn").addEventListener("click", saveReview);
+
+function saveReview() {
+  const title = titleInput.value;
+  const memo = memoInput.value;
+  const star = parseFloat(starInput.value);
+
+  if (!title || !memo) {
+    alert("作品名と感想を入れてね！");
+    return;
+  }
+
+  if (editingIndex === null) reviews.push({ title, memo, star });
+  else {
+    reviews[editingIndex] = { title, memo, star };
+    editingIndex = null;
+  }
+
+  saveToStorage();
+  titleInput.value = "";
+  memoInput.value = "";
+  starInput.value = "5";
+
+  createStars();
+  showReviews();
+}
