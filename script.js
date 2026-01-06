bb8671618f57c388cddcacfa13a1d51eea22aa84
riko303 <riko_line_303@yahoo.co.jp>
2026-01-06 22:01:14 +0900

    script.js を更新

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+reviews = JSON.parse(localStorage.getItem("reviews")) || [];
+
 let reviews = [];
 let editingIndex = null;
 
@@ -13,12 +15,37 @@ function saveReview() {
   const memo = memoInput.value;
 
   if (!title || !memo) {
-    alert("入れてね！");
+    alert("作品名と感想を入れてね！");
     return;
   }
 
   reviews.push({ title, memo });
+
+  // ⭐ これが超大事！！
+  localStorage.setItem("reviews", JSON.stringify(reviews));
+
+  // 入力リセット
+  titleInput.value = "";
+  memoInput.value = "";
 }
+
+function showReviews() {
+  const output = document.getElementById("output");
+  output.innerHTML = "";
+
+  reviews.forEach(r => {
+    const div = document.createElement("div");
+    div.textContent = `${r.title}：${r.memo}`;
+    output.appendChild(div);
+  });
+}
+
+document.getElementById("listBtn").onclick = () => {
+  showReviews();
+  showPage("list");
+};
+
+
 
 function saveAndBack() {
   saveReview();
