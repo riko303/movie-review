2835bbe6a644d69b715dd65c0ea4fddd5725fdf9
riko303 <riko_line_303@yahoo.co.jp>
2026-01-19 14:29:35 +0900

    script.js を更新

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   function showPage(id) {
     document.querySelectorAll(".page").forEach(p => {
-      p.classList.remove("active"));
+      p.classList.remove("active");
     document.getElementById(id).classList.add("active");
   }
 
